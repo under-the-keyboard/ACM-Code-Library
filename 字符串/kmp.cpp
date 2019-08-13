3ffb44fb2caba26dadc0d850ac7082a349de8436
henuyh <2102370669@qq.com>
2019-08-13 09:38:07 +0800

    修改 "哈希"

--- 字符串/kmp.cpp
+++ 字符串/kmp.cpp
@@ -1,30 +1,22 @@
-int a[N], b[N], Next[N];	//从a数组里匹配b数组
-void get_next(int m) {	//求Next数组 
-	Next[0] = -1;	 
-	int i = 0, j = -1;
-	while (i < m) {
-		if(j == -1 || b[i] == b[j]) {
-			Next[++i] = ++j;	//赋值 
-		}else {
-			j = Next[j];	//回溯 
+struct KMP{ // 下标0
+	int nex[maxn];
+	void get_nex(char *buf, int len) {
+		nex[0] = -1;
+		int i = 0, j = -1;
+		while (i < len) {
+			if (j == -1 || buf[i] == buf[j]) nex[++i] = ++j;
+			else j = nex[j];
 		}
 	}
-}
-int kmp(int n, int m) {
-	get_next(m);	//求Next数组 
-	int i = 0, j = 0;
-	int ans = 0;
-	while (i < n) {
-		if (j == -1 || a[i] == b[j]) {	//当前匹配成功进行下一个匹配 
-			i++;
-			j++;	
-		}else {
-			j = Next[j];
+	int get_kmp(char *buf1, char *buf2) { // buf1匹配串，buf2模式串
+		int len1 = strlen(buf1), len2 = strlen(buf2);
+		get_nex(buf2, len2);
+		int cnt = 0, i = 0, j = 0;
+		while (i < len1) {
+			if (j == -1 || buf1[i] == buf2[i]) ++i, ++j;
+			else j = nex[j];
+			if (j == len2) cnt++, j = nex[j];
 		}
-		if (j == m) {	//匹配成功 
-			ans++;
-			j = Next[j];	//进行下一次匹配 
-		}
+		return cnt; // 匹配个数
 	}
-	return ans;
-}
+}kmp;
--- 字符串/最小表示法.cpp
+++ 字符串/最小表示法.cpp
@@ -11,3 +11,18 @@ int minRepresent(char *s, int len) {
     }
     return min(i, j);
 }
+int minRepresent(int start, int end, int len) { // 判断[strat, end]是否为最小表示
+    int i = 0+start, j = 1+start, k = 0;
+    while (i < end && j < end && k < len) {
+        int l = i + k;  if (l >= end) l = l - end + start;
+        int r = j + k;  if (r >= end) r = r - end + start;
+        int t = s[l] - s[r];
+        if (t == 0) k++;
+        else {
+            if (t < 0)  j = max(j+k+1, i+1);
+            else i = max(i+k+1, j+1);
+            k = 0;
+        }
+    }
+    return min(i, j) == start;
+}

46820626e4a5389c21c074d8016de596ef072317
Tony5t4rk <978539910@qq.com>
2019-09-03 13:42:44 +0800

    修改 "矩形面积并,矩形面积异或并", 删除 "二维树状数组"

--- 数据结构/线段树/矩形面积并.cpp
+++ 数据结构/线段树/矩形面积并.cpp
@@ -9,14 +9,14 @@ int Sgn(db k) {
 int Cmp(db k1, db k2) {
   return Sgn(k1 - k2);
 }
-struct seg {
+struct Seg {
   db l, r, h;
   int flag;
 };
-bool operator < (seg &k1, seg &k2) {
+bool operator < (Seg &k1, Seg &k2) {
   return Cmp(k1.h, k2.h) < 0;
 }
-std::vector<seg> segs;
+std::vector<Seg> Segs;
 std::vector<db> pos;
 int BinarySearch(db k) {
   int ret = (int)pos.size() - 1, l = 0, r = (int)pos.size() - 1;
@@ -30,19 +30,19 @@ int BinarySearch(db k) {
   }
   return ret;
 }
-struct node {
+struct Node {
   int l, r, cnt;
   db len;
 };
-node seg_tree[maxn * 10];
+Node Seg_tree[maxn * 10];
 void Pull(int o) {
-  if (seg_tree[o].cnt) seg_tree[o].len = pos[seg_tree[o].r + 1] - pos[seg_tree[o].l];
-  else if (seg_tree[o].l == seg_tree[o].r) seg_tree[o].len = 0.0;
-  else seg_tree[o].len = seg_tree[o << 1].len + seg_tree[o << 1 | 1].len;
+  if (Seg_tree[o].cnt) Seg_tree[o].len = pos[Seg_tree[o].r + 1] - pos[Seg_tree[o].l];
+  else if (Seg_tree[o].l == Seg_tree[o].r) Seg_tree[o].len = 0.0;
+  else Seg_tree[o].len = Seg_tree[o << 1].len + Seg_tree[o << 1 | 1].len;
 }
 void Build(int l, int r, int o) {
-  seg_tree[o].l = l; seg_tree[o].r = r;
-  seg_tree[o].cnt = 0; seg_tree[o].len = 0.0;
+  Seg_tree[o].l = l; Seg_tree[o].r = r;
+  Seg_tree[o].cnt = 0; Seg_tree[o].len = 0.0;
   if (l == r) return;
   int Mid = (l + r) >> 1;
   Build(l, Mid, o << 1);
@@ -50,12 +50,12 @@ void Build(int l, int r, int o) {
   Pull(o);
 }
 void Update(int l, int r, int v, int o) {
-  if (l <= seg_tree[o].l && r >= seg_tree[o].r) {
-    seg_tree[o].cnt += v;
+  if (l <= Seg_tree[o].l && r >= Seg_tree[o].r) {
+    Seg_tree[o].cnt += v;
     Pull(o);
     return;
   }
-  int Mid = (seg_tree[o].l + seg_tree[o].r) >> 1;
+  int Mid = (Seg_tree[o].l + Seg_tree[o].r) >> 1;
   if (r <= Mid) Update(l, r, v, o << 1);
   else if (l > Mid) Update(l, r, v, o << 1 | 1);
   else {
@@ -70,16 +70,16 @@ db x1, y1, x2, y2;
 db ans;
 int main() {
   while (~scanf("%d", &n) && n) {
-    segs.clear();
+    Segs.clear();
     pos.clear();
     for (int i = 0; i < n; ++i) {
       scanf("%lf%lf%lf%lf", &x1, &y1, &x2, &y2);
-      segs.push_back((seg){x1, x2, y1, 1});
-      segs.push_back((seg){x1, x2, y2, -1});
+      Segs.push_back((Seg){x1, x2, y1, 1});
+      Segs.push_back((Seg){x1, x2, y2, -1});
       pos.push_back(x1);
       pos.push_back(x2);
     }
-    std::sort(segs.begin(), segs.end());
+    std::sort(Segs.begin(), Segs.end());
     std::sort(pos.begin(), pos.end(), [&](db k1, db k2) { return Cmp(k1, k2) < 0; });
     int cur = 1;
     for (int i = 1; i < (int)pos.size(); ++i)
@@ -88,10 +88,10 @@ int main() {
     pos.erase(pos.begin() + cur, pos.end());
     Build(0, (int)pos.size(), 1);
     ans = 0.0;
-    for (int i = 0; i < (int)segs.size() - 1; ++i) {
-      int l = BinarySearch(segs[i].l), r = BinarySearch(segs[i].r);
-      Update(l, r - 1, segs[i].flag, 1);
-      ans += (segs[i + 1].h - segs[i].h) * seg_tree[1].len;
+    for (int i = 0; i < (int)Segs.size() - 1; ++i) {
+      int l = BinarySearch(Segs[i].l), r = BinarySearch(Segs[i].r);
+      Update(l, r - 1, Segs[i].flag, 1);
+      ans += (Segs[i + 1].h - Segs[i].h) * Seg_tree[1].len;
     }
     printf("Test case #%d\n", ++cas);
     printf("Total explored area: %.2lf\n\n", ans);

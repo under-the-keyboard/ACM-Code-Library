31dcb327a807cc91857de70dda2547db08998cde
Tony5t4rk <978539910@qq.com>
2019-09-04 15:30:09 +0800

    修改 "莫队算法"

--- 其它/莫队算法/静态莫队.cpp
+++ 其它/莫队算法/静态莫队.cpp
@@ -1,36 +1,40 @@
 const int maxn = "Edit";
+const int maxa = "Edit";
 // 静态莫队算法求区间不同数字数量
 struct MoCap {
   int n, m;
   int block;
   int arr[maxn];
-  struct query { int l, r, id; };
-  query q[maxn];
-  int cnt[maxn << 1];
+  struct Query { int l, r, id; };
+  Query qry[maxn];
+  int cnt[maxa];
   int cur;
   int ans[maxn];
-  void Add(int x) {
-    cur += (++cnt[arr[x]] == 1);
+  void Add(int idx) {
+    cur += (++cnt[arr[idx]] == 1);
   }
-  void Del(int x) {
-    cur -= (--cnt[arr[x]] == 0);
+  void Sub(int idx) {
+    cur -= (--cnt[arr[idx]] == 0);
   }
   void Solve() {
     scanf("%d%d", &n, &m);
-    block = std::sqrt(n);
+    block = std::pow(n, 2. / 3);
     for (int i = 1; i <= n; ++i) scanf("%d%d", &arr[i]);
     for (int i = 1; i <= m; ++i) {
-      scanf("%d%d", &q[i].l, &q[i].r);
-      q[i].id = i;
+      scanf("%d%d", &qry[i].l, &qry[i].r);
+      qry[i].id = i;
     }
-    std::sort(q + 1, q + m + 1, [&](query k1, query k2) { return (k1.l / block) == (k2.l / block) ? k1.r < k2.r : k1.l < k2.l; });
-    int l = 0, r = 0;
+    std::sort(qry + 1, qry + m + 1, [&](Query k1, Query k2) {
+      if (k1.l / block != k2.l / block) return k1.l < k2.l;
+      return k1.r < k2.r;
+    });
+    int l = 1, r = 0;
     for (int i = 1; i <= m; ++i) {
-      while (l < q[i].l) Del(l++);
-      while (l > q[i].l) Add(--l);
-      while (r < q[i].r) Add(++r);
-      while (r > q[i].r) Del(r--);
-      ans[q[i].id] = cur;
+      while (l < qry[i].l) Sub(l++);
+      while (l > qry[i].l) Add(--l);
+      while (r < qry[i].r) Add(++r);
+      while (r > qry[i].r) Sub(r--);
+      ans[qry[i].id] = cur;
     }
     for (int i = 1; i <= m; ++i) printf("%d\n", ans[i]);
   }
